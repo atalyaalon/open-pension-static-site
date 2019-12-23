4bc950481f9d83a6de503e06a93bb58d95c6bcf4
Roy Segall <roy@segall.io>
2019-12-23 20:30:22 +0200

    Init the static site

---
+++ public/js/SheetSwitch.min.fb83f146ad1b1561b3a55c87fbeced0bab0df0c4f4bb2581f4dc485f4a09e8ce.js
@@ -0,0 +1,1 @@
+'use strict';const links=document.querySelectorAll('.nav-tabs .link a');Object.values(links).map(link=>{link.addEventListener("click",(element)=>{document.querySelector('.nav-tabs li.selected').classList.remove('selected');element.target.parentElement.classList.add('selected');})});
