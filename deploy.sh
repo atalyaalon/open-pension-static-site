d64432c26dcac84e23e84731a71fc23f0c51a9b5
Roy Segall <roy@segall.io>
2019-12-24 07:00:56 +0200

    Adding token

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ echo -e "\033[0;32mPassing CNAME to the doc folder\033[0m"
 git add .
 
 git commit -m "Updating site"
-git push origin master
+git push https://roysegall:$token@github.com/hasadna/open-pension-static-site/
 
 echo -e "\033[0;32mDone!\033[0m"
