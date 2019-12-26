b9117cd4eae1bfa87e451ee1a18b61800a563956
Roy Segall <roy@segall.io>
2019-12-26 21:01:52 +0200

    Adding cname

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 # Build the project.
 hugo -t openpension --minify
 echo -e "\033[0;32mPassing CNAME to the doc folder\033[0m"
-#cp CNAME compiles
-#cd compiles
+cp CNAME docs
 git add .
 
 git commit -m "Updating site"
